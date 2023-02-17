3e8165ba685c917447fbb4b709a98226a8342249
Thanh <thanh.dang@sotatek.com>
2023-02-17 03:08:25 +0000

    Update Woman.jsx

--- src/components/Woman.jsx
+++ src/components/Woman.jsx
@@ -8,9 +8,11 @@ import { useCharacterAnimations } from "../contexts/CharacterAnimations";
 
 const Woman = (props) => {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF("./models/woman.gltf");
+  const gltf = useGLTF(
+    "https://d2ok5yp7stnjtq.cloudfront.net/public/output/nft-image/admin/2/1/3/c9862be2-ba1d-4f30-8c53-6dbbdf545c3d_1.gltf"
+  );
   const { setAnimations, animationIndex } = useCharacterAnimations();
-  const { actions, names } = useAnimations(animations, group);
+  const { actions, names } = useAnimations(gltf.animations, group);
 
   useEffect(() => {
     setAnimations(names);
@@ -25,87 +27,9 @@ const Woman = (props) => {
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <group name="Scene">
-        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-          <primitive object={nodes.mixamorigHips} />
-          <group name="SM_Chr_Developer_Female_02">
-            <skinnedMesh
-              castShadow
-              name="Mesh019"
-              geometry={nodes.Mesh019.geometry}
-              material={materials.Glasses}
-              skeleton={nodes.Mesh019.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_1"
-              geometry={nodes.Mesh019_1.geometry}
-              material={materials.Eyes}
-              skeleton={nodes.Mesh019_1.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_2"
-              geometry={nodes.Mesh019_2.geometry}
-              material={materials.Hair}
-              skeleton={nodes.Mesh019_2.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_3"
-              geometry={nodes.Mesh019_3.geometry}
-              material={materials.Skin}
-              skeleton={nodes.Mesh019_3.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_4"
-              geometry={nodes.Mesh019_4.geometry}
-              material={materials.Mouth}
-              skeleton={nodes.Mesh019_4.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_5"
-              geometry={nodes.Mesh019_5.geometry}
-              material={materials.Shirt}
-              skeleton={nodes.Mesh019_5.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_6"
-              geometry={nodes.Mesh019_6.geometry}
-              material={materials.Pants}
-              skeleton={nodes.Mesh019_6.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_7"
-              geometry={nodes.Mesh019_7.geometry}
-              material={materials.Shoes}
-              skeleton={nodes.Mesh019_7.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_8"
-              geometry={nodes.Mesh019_8.geometry}
-              material={materials.Sole}
-              skeleton={nodes.Mesh019_8.skeleton}
-            />
-            <skinnedMesh
-              castShadow
-              name="Mesh019_9"
-              geometry={nodes.Mesh019_9.geometry}
-              material={materials.Laces}
-              skeleton={nodes.Mesh019_9.skeleton}
-            />
-          </group>
-        </group>
-      </group>
+      <primitive object={gltf.scene} />
     </group>
   );
 };
 
 export default Woman;
-
-useGLTF.preload("./models/woman.gltf");
